6941087d177f3d0c11a03a43fe2943a5417ac31a
Gabriel-dot-pixel <marinho.gabriel@aluno.ifsp.edu.br>
2026-07-16 23:35:06 -0300

    corrigi mais alguns exercicios

--- Resoluções dos desafios/ex051 - progressao aritimetica.py
+++ Resoluções dos desafios/ex051 - progressao aritimetica.py
@@ -2,7 +2,7 @@
 print(f'{" DESAFIO 051 ":=^25}')
 i = int(input('Digite o primeiro termo da PA: '))
 r = int(input('Digite a razão da PA: '))
+d = i + (10-1) * r
 print('Os 10 primeiros termos da progressão são:')
-for c in range(1, 11):
-    t = i + (c-1) * r
-    print(t)
+for c in range(i, d + r, r):
+    print(c)
--- Resoluções dos desafios/ex052 - numeros primos.py
+++ Resoluções dos desafios/ex052 - numeros primos.py
@@ -1,9 +1,11 @@
 # Faça um programa que leia um número inteiro e diga se ele é ou não um número primo
-from math import sqrt, floor
 print(f'{" DESAFIO 052 ":=^25}')
-itens = []
 n = int(input('Digite um número: '))
-raiz = floor(sqrt(n))
-#for i in range(3, raiz+1):
-#    if i % 2 == 1:
-        
+t = 0
+for i in range(1, n+1):
+    if n % i == 0:
+        t += 1
+if t == 2:
+    print(f'{n} é primo')
+else:
+    print(f'{n} não é primo')
